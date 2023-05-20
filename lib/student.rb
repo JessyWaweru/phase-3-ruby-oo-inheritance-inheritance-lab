f2efd1e838100f312ef69304907c6bb19d8c1ab9
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-05-21 02:08:50 +0300

    assignment done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
-
+class Student <User
+    KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+attr_accessor :knowledge
+def initialize
+    @knowledge=[]
+    
 end
+def knowledge
+    @knowledge=knowledge
+    
+end
+def learn
+    @knowledge << KNOWLEDGE.sample
+end
+end
+
+sophie=Student.new()
